b65a6c64bcf0bea69d578173a184647c44b8372a
HstarStudio <hm910705@163.com>
2022-09-22 16:13:26 +0800

    feat: use @bizjs/shadow-render-react render html to shadow DOM

--- packages/yuque-doc-render/src/YuqueDocRender/index.tsx
+++ packages/yuque-doc-render/src/YuqueDocRender/index.tsx
@@ -1,62 +1,14 @@
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
+import { ShadowRender } from '@bizjs/shadow-render-react';
+import type { ShadowRenderRef } from '@bizjs/shadow-render-react';
 import { highlightElement } from 'prismjs';
-import 'prismjs/components/prism-bash';
-import 'prismjs/components/prism-basic';
-import 'prismjs/components/prism-c';
-import 'prismjs/components/prism-csharp';
-import 'prismjs/components/prism-cpp';
-import 'prismjs/components/prism-dart';
-import 'prismjs/components/prism-diff';
-import 'prismjs/components/prism-docker';
-import 'prismjs/components/prism-erlang';
-import 'prismjs/components/prism-git';
-import 'prismjs/components/prism-go';
-import 'prismjs/components/prism-graphql';
-import 'prismjs/components/prism-groovy';
-import 'prismjs/components/prism-http';
-import 'prismjs/components/prism-java';
-import 'prismjs/components/prism-json';
-import 'prismjs/components/prism-jsx';
-import 'prismjs/components/prism-kotlin';
-import 'prismjs/components/prism-latex';
-import 'prismjs/components/prism-less';
-import 'prismjs/components/prism-lua';
-import 'prismjs/components/prism-matlab';
-import 'prismjs/components/prism-makefile';
-import 'prismjs/components/prism-markdown';
-import 'prismjs/components/prism-nginx';
-import 'prismjs/components/prism-objectivec';
-// 被 php 依赖
-import 'prismjs/components/prism-markup-templating';
-import 'prismjs/components/prism-php';
-// 必须放在 plsql 之前，被 plsql 依赖
-import 'prismjs/components/prism-sql';
-import 'prismjs/components/prism-plsql';
-import 'prismjs/components/prism-pascal';
-import 'prismjs/components/prism-perl';
-import 'prismjs/components/prism-powershell';
-import 'prismjs/components/prism-protobuf';
-import 'prismjs/components/prism-python';
-import 'prismjs/components/prism-r';
-import 'prismjs/components/prism-ruby';
-import 'prismjs/components/prism-rust';
-import 'prismjs/components/prism-scala';
-import 'prismjs/components/prism-swift';
-import 'prismjs/components/prism-systemd';
-import 'prismjs/components/prism-tcl';
-import 'prismjs/components/prism-typescript';
-import 'prismjs/components/prism-tsx';
-import 'prismjs/components/prism-vbnet';
-import 'prismjs/components/prism-verilog';
-import 'prismjs/components/prism-velocity';
-import 'prismjs/components/prism-yaml';
-// "KaTeX", "Vue", "sTeX" 不存在
-
-import { HtmlRender, HtmlRenderRef } from '../html-render-react/HtmlRender';
+// 语言包
+import './prism-languages';
 
 export type YuqueDocRenderProps = {
   className?: string;
   docHtml: string;
+  codeTheme?: string;
 };
 
 const styles = [
@@ -67,12 +19,12 @@ const styles = [
 export function YuqueDocRender(props: YuqueDocRenderProps) {
   const { docHtml, className } = props;
 
-  const htmlRenderRef = useRef<HtmlRenderRef>(null);
+  const shadowRenderRef = useRef<ShadowRenderRef>(null);
 
   useEffect(() => {
     setTimeout(() => {
-      if (htmlRenderRef.current) {
-        const contentDom: HTMLDivElement = htmlRenderRef.current.getContentDOM();
+      if (shadowRenderRef.current) {
+        const contentDom: HTMLDivElement = shadowRenderRef.current.getContentDOM();
         contentDom.querySelectorAll('pre.ne-codeblock').forEach((item) => {
           highlightElement(item);
         });
@@ -80,5 +32,5 @@ export function YuqueDocRender(props: YuqueDocRenderProps) {
     });
   }, [docHtml]);
 
-  return <HtmlRender htmlContent={docHtml} ref={htmlRenderRef} className={className} styles={styles} />;
+  return <ShadowRender htmlContent={docHtml} ref={shadowRenderRef} className={className} styles={styles} />;
 }
